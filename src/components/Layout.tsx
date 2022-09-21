c90b6d344898618d28d06d3493c98fc94a8e8ce8
r31sr4r <rafael.reis.a@gmail.com>
2022-09-20 22:11:39 -0300

    feat: first commit

---
+++ src/components/Layout.tsx
@@ -0,0 +1,12 @@
+import { Box, Container } from "@mui/material";
+
+
+export function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <Box>
+        <Container maxWidth="lg" sx={{ mt:4, mb:4, backgroundColor: '#000' }}>
+            {children}
+        </Container>
+    </Box>
+  )
+}
